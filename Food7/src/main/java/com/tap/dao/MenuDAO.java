b0071367f5ab1efc23242b82218dc8e470e32034
Nayab Mutaza <nayabmurtaza4@gmail.com>
2026-07-02 19:08:38 +0530

    Delete Food7 directory

--- Food7/src/main/java/com/tap/dao/MenuDAO.java
+++
@@ -1,9 +0,0 @@
-package com.tap.dao;
-
-import java.util.List;
-import com.tap.model.Menu;
-
-public interface MenuDAO {
-    List<Menu> getAllMenusByRestaurant(int restaurantId);
-    Menu getMenu(int menuId);
-}
